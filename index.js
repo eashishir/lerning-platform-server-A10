581edc277ec158f71d50855d514a6a443ddfa2a3
eashishir <eftekharahmedshishir@gmail.com>
2022-10-26 13:54:31 -0700

    index.js work complite

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./Data/categories.json')
+const courses = require('./Data/Courses.json')
 
 app.get('/', (req, res) => {
     res.send ('Js API is Running');
@@ -13,6 +14,33 @@ app.get('/', (req, res) => {
 
 app.get('/js-categories', (req, res) => {
     res.send(categories)
+});
+
+
+app.get ('/js-category/:id', (req,res)=>{
+    const id = req.params.id;
+    if(id === '08'){
+        res.send (courses)
+    }
+   else{
+    const category_course  = courses.find(n => n.category_id === id);
+    res.send (category_course)
+   };
+
+    
+    
+});
+
+app.get('/courses',(req, res) => {
+    res.send(courses)
+});
+
+
+app.get('/courses/:id',(req,res) => {
+    const id = req.params.id;
+    const selectedNews =courses.find(n => n._id === id);
+    res.send(selectedNews);
+    
 });
 
 
