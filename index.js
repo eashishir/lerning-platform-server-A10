7088dd8fa6cb4a2f000f38386a53d9669233c1a8
eashishir <eftekharahmedshishir@gmail.com>
2022-10-26 02:44:43 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./Data/categories.json')
+
+app.get('/', (req, res) => {
+    res.send ('Js API is Running');
+});
+
+app.get('/js-categories', (req, res) => {
+    res.send(categories)
+});
+
+
+app.listen(port, () => {
+    console.log('js learnSmart server is running on port', port);
+});
